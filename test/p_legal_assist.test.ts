6b898917ccf98520f34d9d7c2da029d8adc649ff
Jahnavi Yalamanchili <rakijanu@Jahnavis-MacBook-Air.local>
2025-02-16 19:27:16 -0500

    Initial commit

---
+++ test/p_legal_assist.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PLegalAssist from '../lib/p_legal_assist-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/p_legal_assist-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PLegalAssist.PLegalAssistStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
